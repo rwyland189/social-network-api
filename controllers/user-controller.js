de5569d7eee5139103bea58c3b24e9be75c07836
Becca Wyland <rwyland@uwalumni.com>
2021-07-16 20:29:22 -0500

    create method to update an existing user by id

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -35,6 +35,19 @@ const userController = {
             .then(dbUserData => res.json(dbUserData))
             .catch(err => res.status(400).json(err));
     },
+
+    // update an existing user by id
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id :(' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
 };
 
 module.exports = userController;
