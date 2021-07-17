479e3261d56400e002c6884e036d0cff03322828
Becca Wyland <rwyland@uwalumni.com>
2021-07-16 20:16:48 -0500

    create get all users method

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,7 +2,15 @@ const { User } = require('../models');
 
 // create user functionality
 const userController = {
-    
+    // get all users
+    getAllUsers(req, res) {
+        User.find({})
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
 };
 
 module.exports = userController;
