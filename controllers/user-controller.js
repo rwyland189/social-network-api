cb24ba059ecfecca9a58115909ef0479d80f0232
Becca Wyland <rwyland@uwalumni.com>
2021-07-16 20:23:54 -0500

    create method to add a new user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -28,6 +28,13 @@ const userController = {
                 res.status(400).json(err);
             });
     },
+
+    // create a new user
+    createUser({ body }, res) {
+        User.create(body)
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.status(400).json(err));
+    },
 };
 
 module.exports = userController;
