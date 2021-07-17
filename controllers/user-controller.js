35893fadc6d220184f28f477b794b6c895dc2e99
Becca Wyland <rwyland@uwalumni.com>
2021-07-16 20:33:06 -0500

    create method to delete a user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -48,6 +48,19 @@ const userController = {
             })
             .catch(err => res.status(400).json(err));
     },
+
+    // delete a user from the database
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id :(' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.status(400).json(err));
+    }
 };
 
 module.exports = userController;
