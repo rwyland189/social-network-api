88d188b6ca3b240c88e0859ec60beff5d08cc96c
Becca Wyland <rwyland@uwalumni.com>
2021-07-19 17:09:14 -0500

    restructure getUserById()

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -21,6 +21,11 @@ const userController = {
     // get one user by id
     getUserById({ params }, res) {
         User.findOne({ _id: params.id })
+            .populate({
+                path: 'thoughts',
+                select: '-__v'
+            })
+            .select('-__v')
             .then(dbUserData => {
                 // if no user found, send 404
                 if (!dbUserData) {
